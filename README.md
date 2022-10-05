7693945c74f3a9ec3b46b6a54d8d8e1333c92b2d
kokchuan101 <chuanfrost98@gmail.com>
2022-10-05 18:48:28 +0800

    #70 Updated readme

--- README.md
+++ README.md
@@ -1,18 +1,51 @@
 # damenu
-QR Digital Menu Maker
+QR Digital Menu Maker crafted with NestJS and VueJS.
 
 ## Prerequisite
 - Node.js
+- Docker
+- Docker Compose v2
 
 ## Setup
 
-1. Clone this repository
-2. Run the command below
+Initial Setup
+```
+# Install lerna & husky
+npm install
+```
+
+There are 2 mode to run this project.
+
+#### 1. Docker mode
 
 ```bash
-cd damenu
+# Ensure you are in root directory
+
+# Setup .env
+bash development.sh docker
+
+# Spin up backend and mongo container
+docker compose up -d
+
+# Boot up frontend
+cd frontend
 npm install
-bash development.sh
+npm run serve
+```
+
+Note: Eventhough `lerna` is not utilized for initialization in `docker` mode, it is used for `husky` hook to perform code sniffing both repository on `git commit` .
+
+#### 2. Monorepo mode
+
+```bash
+# Ensure you are in root directory
+
+# Setup .env
+bash development.sh monorepo
+
+# Install npm packages for both frontend & backend
 lerna exec npm install
+
+# Boot up both frontend & backend
 npm run serve
 ```
--- development.sh
+++ development.sh
@@ -1,5 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+mode=$1
+
+if [ "$#" -ne 1 ]; then
+    echo "Invalid number of arguments. Example: ./restore_mongo.sh {mode=monorepo/docker}"
+    exit 1
+fi
+
+if [[ $mode != 'monorepo' && $mode != 'docker' ]]; then
+    echo "Invalid mode, only monorepo or docker is allowed."
+    exit 1
+fi
+
 cp frontend/.env.sample frontend/.env.development
 cp backend/.env.example backend/.env
 
+# Remove these 2 folder when switching mode because:
+# Docker mode create them in root user
+# Monorepo mode create then in non-root user mode
+# TODO: Fix Docker to create with non-root user (hehe)
+sudo rm -rf backend/node_modules backend/node_modules/dist
+
 sed -i 's/{API_HOST}/http:\/\/localhost:3000/' frontend/.env.development
-sed -i 's/{MONGO_URL}/mongodb:\/\/damenu-mongo:27017\/damenu/' backend/.env
+
+if [[ $mode = 'monorepo' ]]; then
+    sed -i 's/{MONGO_URL}/mongodb:\/\/127.0.0.1:27017\/damenu/' backend/.env
+elif [[ $mode = 'docker' ]]; then
+    sed -i 's/{MONGO_URL}/mongodb:\/\/damenu-mongo:27017\/damenu/' backend/.env
+fi
