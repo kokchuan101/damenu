c281d0eb8a9b9fd7ee2655bd7bf05e7b59940cd0
kokchuan101 <chuanfrost98@gmail.com>
2022-10-05 17:31:51 +0800

    #70 Updated .env

--- development.sh
+++ development.sh
@@ -1,3 +1,5 @@
 cp frontend/.env.sample frontend/.env.development
+cp backend/.env.example backend/.env
 
 sed -i 's/{API_HOST}/http:\/\/localhost:3000/' frontend/.env.development
+sed -i 's/{MONGO_URL}/mongodb:\/\/damenu-mongo:27017\/damenu/' backend/.env
