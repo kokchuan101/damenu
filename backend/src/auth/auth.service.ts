6009f0a97a806a2aaba281232da25355c6e6510f
kokchuan101 <chuanfrost98@gmail.com>
2022-10-05 17:56:49 +0800

    #70 Added nest config & fix login bug, LOL

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -5,30 +5,32 @@ import { Account } from 'src/accounts/schema/account.schema';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService
-{
+export class AuthService {
     constructor(
         private accountsService: AccountsService,
         private jwtService: JwtService
-    ) { }
+    ) {}
 
-    async validateUser(email: string, password: string): Promise<Account>
-    {
+    async validateUser(email: string, password: string): Promise<Account> {
         const account: Account = await this.accountsService.findOne(email);
-        const passMatch: boolean = await bcrypt.compare(password, account.password);
-        delete account.password;
-        if (account && passMatch)
-        {
-            return account;
-        } else
-        {
+        if (!account) {
             return null;
         }
+
+        const passMatch: boolean = await bcrypt.compare(
+            password,
+            account.password
+        );
+        delete account.password;
+        if (passMatch) {
+            return account;
+        }
+
+        return null;
     }
 
     // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    async login(user: any)
-    {
+    async login(user: any) {
         const payload: any = { username: user.email, sub: user.userId };
         return {
             access_token: this.jwtService.sign(payload),
